f05d37718b9b35b8e1a8b5ae125383d0f11b4782
Connor Shea <connor.james.shea@gmail.com>
2022-06-20 10:24:11 -0600

    Fill out more of the methods on Item.

--- lib/wikidatum/item.rb
+++ lib/wikidatum/item.rb
@@ -5,9 +5,15 @@ class Wikidatum::Item
   # @param descriptions [Array<Wikidatum::Term>] An array of {Wikidatum::Term}s representing descriptions for this item.
   # @param aliases [Array<Wikidatum::Term>] An array of {Wikidatum::Term}s representing aliases for this item.
   # @param statements [Array<Wikidatum::Statement>] Statements for this item.
-  # @param sitelinks [Array<Wikidatum::Sitelink>]
+  # @param sitelinks [Array<Wikidatum::Sitelink>] Sitelinks for this item.
   # @return [Wikidatum::Item]
-  def initialize(labels:, descriptions:, aliases:, statements:, sitelinks:); end
+  def initialize(labels:, descriptions:, aliases:, statements:, sitelinks:)
+    @labels = labels
+    @descriptions = descriptions
+    @aliases = aliases
+    @statements = statements
+    @sitelinks = sitelinks
+  end
 
   # @param property [String] Wikidata property, in the format of 'P123'. If unspecified, will return all statements for the item.
   # @return [Array<Wikidatum::Statement>]
@@ -16,37 +22,41 @@ class Wikidatum::Item
   # @param lang [String, Symbol]
   # @return [Wikidatum::Term, nil]
   def label(lang:)
-    labels.find { |label| label.lang == lang.to_s }
+    @labels.find { |label| label.lang == lang.to_s }
   end
 
   # @param langs [Array<String, Symbol>]
   # @return [Array<Wikidatum::Term>]
-  def labels(langs: []); end
+  def labels(langs: [])
+    return @labels if langs.empty?
+
+    langs.map!(&:to_s)
+    @labels.filter { |label| langs.include?(label.lang) }
+  end
 
   # @param lang [String, Symbol]
   # @return [Wikidatum::Term, nil]
   def description(lang:)
-    descriptions.find { |desc| desc.lang == lang.to_s }
+    @descriptions.find { |desc| desc.lang == lang.to_s }
   end
 
   # @param langs [Array<String, Symbol>]
   # @return [Array<Wikidatum::Term>]
-  def descriptions(langs: []); end
+  def descriptions(langs: [])
+    return @descriptions if langs.empty?
+
+    langs.map!(&:to_s)
+    @descriptions.filter { |desc| langs.include?(desc.lang) }
+  end
 
   # @param langs [Array<Symbol, String>] If unspecified, will return all aliases for all languages.
   # @return [Array<Wikidatum::Term>]
-  def aliases(langs: []); end
+  def aliases(langs: [])
+    return @aliases if langs.empty?
 
-  # Get the sitelinks on the item.
-  #
-  # @example Getting all sitelinks for the item.
-  #   item.sitelinks
-  # @example Getting only a few specific sitelinks.
-  #   item.sitelink(sites: ['enwiki', 'eswiki', 'commons'])
-  #
-  # @param sites [Array<String, Symbol>] An array of sitelink shortcodes to return (e.g. ['enwiki', 'eswiki']), if not provided then all sitelinks will be returned.
-  # @return [Array<Wikidatum::Sitelink>]
-  def sitelinks(sites: []); end
+    langs.map!(&:to_s)
+    @aliases.filter { |al| langs.include?(al.lang) }
+  end
 
   # Get a specific sitelink based on its shortcode.
   #
@@ -58,6 +68,22 @@ class Wikidatum::Item
   # @param site [String, Symbol] The shortcode for the sitelink you want to access, e.g. 'enwiki' or 'commons'. Can be a string or a symbol.
   # @return [Wikidatum::Sitelink, nil]
   def sitelink(site:)
-    sitelinks.find { |sitelink| sitelink.site == site.to_s }
+    @sitelinks.find { |sitelink| sitelink.site == site.to_s }
+  end
+
+  # Get the sitelinks on the item.
+  #
+  # @example Getting all sitelinks for the item.
+  #   item.sitelinks
+  # @example Getting only a few specific sitelinks.
+  #   item.sitelink(sites: ['enwiki', 'eswiki', 'commons'])
+  #
+  # @param sites [Array<String, Symbol>] An array of sitelink shortcodes to return (e.g. ['enwiki', 'eswiki']), if not provided then all sitelinks will be returned.
+  # @return [Array<Wikidatum::Sitelink>]
+  def sitelinks(sites: [])
+    return @sitelinks if sites.empty?
+
+    sites.map!(&:to_s)
+    @sitelinks.filter { |sitelink| sites.include?(sitelink.site) }
   end
 end
