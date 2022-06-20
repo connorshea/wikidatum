baf37fdc64304b6d961fba61c4f66cbc29522ec8
Connor Shea <connor.james.shea@gmail.com>
2022-06-19 19:25:34 -0600

    Add some basic structure for the Client and Item classes.

--- lib/wikidatum/client.rb
+++ lib/wikidatum/client.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class Wikidatum::Client
+  attr_reader :bot
+
+  # @param bot [Boolean] Whether requests sent by this client instance should
+  #                      be registered as bot requests.
+  # @return [Wikidatum::Client]
+  def initialize(bot: true)
+    @bot = bot
+  end
 end
--- lib/wikidatum/item.rb
+++ lib/wikidatum/item.rb
@@ -1,2 +1,28 @@
+# frozen_string_literal: true
+
 class Wikidatum::Item
+  # @param property [String] Wikidata property, in the format of 'P123'. If unspecified, will return all statements for the item.
+  # @return [Array<Wikidatum::Statement>]
+  def statements(property: nil); end
+
+  # @param language [String, Symbol]
+  # @return [Wikidatum::Term]
+  def label(language); end
+
+  # @param languages [Array<String, Symbol>]
+  # @return [Array<Wikidatum::Term>]
+  def labels(languages = []); end
+
+  # @return [Wikidatum::Term]
+  def description(language); end
+
+  # @return [Array<Wikidatum::Term>]
+  def descriptions(languages = []); end
+
+  # @param languages [Array<Symbol, String>] If unspecified, will return all aliases for all languages.
+  # @return [Array<Wikidatum::Term>]
+  def aliases(languages = []); end
+
+  # @return [Array<Wikidatum::Sitelink>]
+  def sitelinks; end
 end
