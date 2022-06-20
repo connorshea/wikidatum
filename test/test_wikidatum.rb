aa2ef38576a17d0e7e98d2f20cc610c25897b5bf
Connor Shea <connor.james.shea@gmail.com>
2022-06-20 10:47:54 -0600

    Add a few tests.

--- test/test_wikidatum.rb
+++ test/test_wikidatum.rb
@@ -6,8 +6,4 @@ class TestWikidatum < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Wikidatum::VERSION
   end
-
-  def test_it_does_something_useful
-    assert true
-  end
 end
