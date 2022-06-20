bc705205137387587830a1954c3d5d36c9af2644
Connor Shea <connor.james.shea@gmail.com>
2022-06-20 10:07:28 -0600

    Fill out the Sitelink class.

--- lib/wikidatum/sitelink.rb
+++ lib/wikidatum/sitelink.rb
@@ -1,4 +1,24 @@
 # frozen_string_literal: true
 
+# Wikidatum::Sitelinks represent associated sitelinks on a Wikidata item.
+# For example, the sitelinks have 
 class Wikidatum::Sitelink
+  # @return [String]
+  attr_reader :site
+
+  # @return [String]
+  attr_reader :title
+
+  # @return [Array<String>]
+  attr_reader :badges
+
+  # @param site [String, Symbol] The shortcode for the given site (e.g. 'enwiki', 'commons', etc.), can be either a string or a symbol.
+  # @param title [String] The title of the page in the associated Wikimedia site.
+  # @param badges [Array<String>] An array of badge names, optional.
+  # @return [Wikidatum::Sitelink]
+  def initialize(site:, title:, badges: [])
+    @site = site.to_s
+    @title = title
+    @badges = badges
+  end
 end
