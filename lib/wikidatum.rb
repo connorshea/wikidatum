27b90304df8b5456c5d8ca3cb4458b3a85596f98
Connor Shea <connor.james.shea@gmail.com>
2022-06-20 11:55:28 -0600

    Add some convenience constants for language codes.

--- lib/wikidatum.rb
+++ lib/wikidatum.rb
@@ -11,4 +11,32 @@ require_relative 'wikidatum/version'
 
 module Wikidatum
   class Error < StandardError; end
+
+  # These language codes are not enforced, you can pass whatever language code
+  # you want, even if it's not represented in this list. The purpose of this
+  # is to provide helper constants to allow writing more readable code.
+  #
+  # This will only cover some of the most common language codes, not all of
+  # them.
+  module LanguageCodes
+    Arabic = 'ar'
+    BrazilianPortuguese = 'pt-br'
+    Chinese = 'zh'
+    Dutch = 'nl'
+    English = 'en'
+    EnglishUK = 'en-gb'
+    French = 'fr'
+    German = 'de'
+    Hebrew = 'he'
+    Hindi = 'hi'
+    Italian = 'it'
+    Polish = 'pl'
+    Portuguese = 'pt'
+    Russian = 'ru'
+    SimplifiedChinese = 'zh-hans'
+    Spanish = 'es'
+    TraditionalChinese = 'zh-hant'
+    Turkish = 'tr'
+    Ukrainian = 'uk'
+  end
 end
