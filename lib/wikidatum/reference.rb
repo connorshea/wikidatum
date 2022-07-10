441b16c0a9ec79b8adc32dcc16c4d8c5f1855dfd
Connor Shea <connor.james.shea@gmail.com>
2022-07-10 13:59:54 -0600

    Fill out the Reference class a bit.

--- lib/wikidatum/reference.rb
+++ lib/wikidatum/reference.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class Wikidatum::Reference
+  # @return [String] Hash of the reference (a cryptographic hash, not a Ruby hash).
+  attr_reader :hash
+
+  # @return [Array<Wikidatum::Snak>]
+  attr_reader :snaks
+
+  # @!visibility private
+  # @param hash [String] Hash of the reference (a cryptographic hash, not a Ruby hash).
+  # @param snaks [Array<Wikidatum::Snak>]
+  def initialize(hash:, snaks:)
+    @hash = hash
+    @snaks = snaks
+  end
 end
