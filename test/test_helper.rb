b2e9a95e18b4e092f05539a768a3e23ed3ce6265
Connor Shea <connor.james.shea@gmail.com>
2022-08-12 15:59:47 -0600

    Improve Simplecov config.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,10 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
 require 'simplecov'
-SimpleCov.start
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter "/test/"
+end
 
 require "wikidatum"
 
