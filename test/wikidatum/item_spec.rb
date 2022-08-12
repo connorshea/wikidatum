ea8c54c4cde58d24d970e8d154c5e99a1c5c1590
Connor Shea <connor.james.shea@gmail.com>
2022-08-12 16:20:13 -0600

    Add somevalue and novalue to the Q123 fixture.

--- test/wikidatum/item_spec.rb
+++ test/wikidatum/item_spec.rb
@@ -10,4 +10,20 @@ describe Wikidatum::Item do
       assert item.is_a?(Wikidatum::Item)
     end
   end
+
+  describe '#to_h' do
+    it 'works with an item that has many types of datavalues' do
+      item = Wikidatum::Item.marshal_load(JSON.parse(File.read('test/fixtures/q123.json')))
+
+      assert item.to_h.is_a?(Hash)
+    end
+  end
+
+  describe '#inspect' do
+    it 'works with an item that has many types of datavalues' do
+      item = Wikidatum::Item.marshal_load(JSON.parse(File.read('test/fixtures/q123.json')))
+
+      assert item.inspect.is_a?(String)
+    end
+  end
 end
