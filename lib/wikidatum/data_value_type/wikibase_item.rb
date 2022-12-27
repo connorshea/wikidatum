f47a8951c5358a077d43be694a688726be887e79
Connor Shea <connor.james.shea@gmail.com>
2022-12-27 15:43:30 -0700

    Rework WikibaseItem class.
    Remove numeric ID and Entity Type, rename id to wikibase_id.
    
    And update the example JSON doc.

--- lib/wikidatum/data_value_type/wikibase_item.rb
+++ lib/wikidatum/data_value_type/wikibase_item.rb
@@ -2,46 +2,34 @@
 
 require 'wikidatum/data_value_type/base'
 
-# The Wikibase Item type datavalue JSON looks like this:
+# The Wikibase Item type JSON looks like this:
 #
 # ```json
 # {
-#   "datavalue": {
-#     "value": {
-#       "entity-type": "item",
-#       "numeric-id": 552863,
-#       "id": "Q552863"
-#     },
-#     "type": "wikibase-item"
+#   "property": {
+#     "id": "P963",
+#     "data-type": "wikibase-item"
+#   },
+#   "value": {
+#     "type": "value",
+#     "content": "Q524026"
 #   }
 # }
 # ```
 class Wikidatum::DataValueType::WikibaseItem
-  # @return [String] usually "wikibase-item"
-  attr_reader :entity_type
-
-  # @return [Integer] the integer representation of the Wikibase ID.
-  attr_reader :numeric_id
-
   # @return [String] in the format "Q123".
-  attr_reader :id
+  attr_reader :wikibase_id
 
-  # @param entity_type [String]
-  # @param numeric_id [Integer]
-  # @param id [String]
+  # @param wikibase_id [String]
   # @return [void]
-  def initialize(entity_type:, numeric_id:, id:)
-    @entity_type = entity_type
-    @numeric_id = numeric_id
-    @id = id
+  def initialize(wikibase_id:)
+    @wikibase_id = wikibase_id
   end
 
   # @return [Hash]
   def to_h
     {
-      entity_type: @entity_type,
-      numeric_id: @numeric_id,
-      id: @id
+      wikibase_id: @wikibase_id
     }
   end
 
@@ -53,13 +41,11 @@ class Wikidatum::DataValueType::WikibaseItem
   end
 
   # @!visibility private
-  def self.marshal_load(data_value_json)
+  def self.marshal_load(wikibase_id)
     Wikidatum::DataValueType::Base.new(
       type: :wikibase_item,
       value: new(
-        entity_type: data_value_json['entity-type'],
-        numeric_id: data_value_json['numeric-id'],
-        id: data_value_json['id']
+        wikibase_id: wikibase_id
       )
     )
   end
@@ -67,9 +53,7 @@ class Wikidatum::DataValueType::WikibaseItem
   # @!visibility private
   def marshal_dump
     {
-      'entity-type': @entity_type,
-      'numeric-id': @numeric_id,
-      id: @id
+      wikibase_id: @wikibase_id
     }
   end
 end
