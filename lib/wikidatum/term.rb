6f63aa2ec24d829db7d80ff7e7d307139af56d2f
Connor Shea <connor.james.shea@gmail.com>
2022-12-29 11:47:23 -0700

    Update documentation for `Wikidatum::Term` class.

--- lib/wikidatum/term.rb
+++ lib/wikidatum/term.rb
@@ -3,16 +3,16 @@
 # Wikidatum::Term represents "Terms", which are pairs of language codes and
 # values. They're used for things like item labels, descriptions, and aliases.
 class Wikidatum::Term
-  # @return [String]
+  # @return [String] A language code ('en', 'zh-mo', etc).
   attr_reader :lang
 
-  # @return [String]
+  # @return [String] The value of the Term.
   attr_reader :value
 
   # @!visibility private
   #
   # @param lang [String, Symbol] A language code ('en', 'zh-mo', etc), can be a symbol or a string.
-  # @param value [String] The value of the term.
+  # @param value [String] The value of the Term.
   # @return [Wikidatum::Term]
   def initialize(lang:, value:)
     @lang = lang.to_s
